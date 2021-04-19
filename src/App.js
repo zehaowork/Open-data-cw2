fdb28e32562c1463d2ea48fc613ff0495f1b0c52
xhao98 <zehaowork@outlook.com>
2021-04-19 20:51:35 +0100

    Router Setup Complete.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
+import React from "react";
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Donate from './pages/Donate/Donate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/donate">Donate</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact"  component={Contact} />
+          <Route path="/donate" component={Donate} />
+         
+        
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+export default function Nav(){
+    return(
+        <nav>
+            <h3>
+                LOGO
+            </h3>
+            <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        About
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+            </ul>
+        </nav>
+    );
+};
