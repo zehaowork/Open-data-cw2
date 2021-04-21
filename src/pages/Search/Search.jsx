ee85f3699c3e6f2021e95cf311135b29e4ff46e9
xhao98 <zehaowork@outlook.com>
2021-04-21 01:34:47 +0100

    static search page completed.

---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,170 @@
+import React from 'react';
+import './Search.css';
+import 'antd/dist/antd.css';
+import { Input, DatePicker,Select,Typography,InputNumber,Divider,Button,List,Space } from 'antd';
+import { CarOutlined,UserOutlined } from '@ant-design/icons';
+const { Title } = Typography;
+export default  function Search(){
+    const IconText = ({ icon, text }) => (
+        <Space>
+          {React.createElement(icon)}
+          {text}
+        </Space>
+      );
+    
+    const data = [{
+        href: 'https://ant.design',
+        title: `ant design part `,
+        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        description:
+          'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+        content:
+          'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      },
+      {
+        href: 'https://ant.design',
+        title: `ant design part`,
+        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        description:
+          'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+        content:
+          'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      },
+      {
+        href: 'https://ant.design',
+        title: `ant design part`,
+        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        description:
+          'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+        content:
+          'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      },
+      {
+        href: 'https://ant.design',
+        title: `ant design part`,
+        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        description:
+          'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+        content:
+          'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      }
+    ]
+
+      
+        
+        
+
+    return (<div className='content'>
+        <div className='search'>
+        <Title level={3} >Project Search</Title>
+        <div className='entry'>
+            <div>
+            Project Name:
+            </div>
+            <Input.Search placeholder="Search project." style={{width:'60%'}} enterButton />
+        </div>
+        <div className='entry'>
+            <div>
+            Start Date:
+            </div>
+            <DatePicker  />
+        </div>
+        <div className='entry'>
+            <div>
+            End Date:
+            </div>
+            <DatePicker  />
+        </div>
+        <div className='entry'>
+            <div>
+            Country:
+            </div>
+            <Select placeholder="Country" />
+        </div>
+        <div className='entry'>
+            <div>
+            Category:
+            </div>
+            <Select placeholder="Category" />
+        </div>
+       
+        <Title level={5} >Donations Goal</Title>
+       
+        <div className='entry'>
+            <div>
+            From:
+            </div>
+            <InputNumber 
+            formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            parser={value => value.replace(/\$\s?|(,*)/g, '')} />
+        </div>
+        <div className='entry'>
+            <div>
+            To:
+            </div>
+            <InputNumber 
+            formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            parser={value => value.replace(/\$\s?|(,*)/g, '')} />
+        </div>
+        <div className='entry'>
+            <div>
+            Amount Contributed:
+            </div>
+            <InputNumber 
+            formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            parser={value => value.replace(/\$\s?|(,*)/g, '')} />
+        </div>
+        <Title level={5} >Number of Participants</Title>
+       
+       <div className='entry'>
+           <div>
+           From:
+           </div>
+           <InputNumber 
+           formatter={value => `${value}ppl`}
+           parser={value => value.replace('ppl', '')} />
+       </div>
+       <div className='entry'>
+           <div>
+           To:
+           </div>
+           <InputNumber 
+           formatter={value => `${value}ppl`}
+           parser={value => value.replace('ppl', '')} />
+       </div>
+       <Button block={true} type="primary" size='large'>Search</Button>
+        </div>
+        <Divider style={{height:'100%'}} type='vertical' />
+        <div className='result'>
+        <Title level={3} >Search Result</Title>
+        <List dataSource={data} 
+        itemLayout="vertical"
+        size="large"
+        renderItem={item => (
+            <List.Item
+            extra={
+                <img
+                  width={272}
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                />
+              }
+              key={item.title}
+              actions={[
+                <IconText icon={CarOutlined} text="156km" key="list-vertical-star-o" />,
+                <IconText icon={UserOutlined} text="200,000ppl" key="list-vertical-star-o" />
+              ]}
+              
+            >
+              <List.Item.Meta
+                title={<a href={item.href}>{item.title}</a>}
+                description={item.description}
+              />
+              {item.content}
+            </List.Item>
+          )}/ >
+        
+    
+        </div>
+    </div>)
+}
