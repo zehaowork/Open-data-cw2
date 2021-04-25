e11162fb66f2c2c63855becc07995273d60f3398
xhao98 <zehaowork@outlook.com>
2021-04-25 16:24:38 +0100

    First API completed.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import {Switch,Route,Link,Redirect} from "react-router-dom";
 
 import 'antd/dist/antd.css';
 import './App.css';
@@ -19,31 +19,31 @@ const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <Router>
+    
   <Layout style={{height:"100vh"}}>
   <Header>
       <img className='logo' alt='Charitable' src={logoIcon} />
   <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
 
-        <Menu.Item key="1"> <Link to="/">Home</Link></Menu.Item>
-        <Menu.Item key="2"><Link to="/about">About</Link></Menu.Item>
-        <Menu.Item key="3"><Link to="/contact">Contact</Link></Menu.Item>
-        <Menu.Item key="4"><Link to="/donate">Donate</Link></Menu.Item>
-        <Menu.Item key="5"><Link to="/search">Search</Link></Menu.Item>
+        <Menu.Item key="1"> <Link to="/Open-data-cw2">Home</Link></Menu.Item>
+        <Menu.Item key="2"><Link to="/Open-data-cw2/about">About</Link></Menu.Item>
+        <Menu.Item key="3"><Link to="/Open-data-cw2/contact">Contact</Link></Menu.Item>
+        <Menu.Item key="4"><Link to="/Open-data-cw2/donate">Donate</Link></Menu.Item>
+        <Menu.Item key="5"><Link to="/Open-data-cw2/search">Search</Link></Menu.Item>
       </Menu>
   </Header>
   <Content style ={{width:'100%',background:'white'}}>
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact"  component={Contact} />
-          <Route path="/donate" component={Donate} />
-          <Route path="/search" component={Search} />
+          <Route path="/Open-data-cw2" exact component={Home} />
+          <Route path="/Open-data-cw2/about" component={About} />
+          <Route path="/Open-data-cw2/contact"  component={Contact} />
+          <Route path="/Open-data-cw2/donate" component={Donate} />
+          <Route path="/Open-data-cw2/search" component={Search} />
         </Switch>
   </Content>
   <Footer style={{ textAlign: 'center' }}>Charitable ©2021 Created by Group 9</Footer>
   </Layout>
-  </Router>
+  
     
 
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Button, Space } from 'antd';
 import './Home.css';
+import axios from 'axios'
 
 export default function Home(){
+   
+    const testApi = ()=>{
+        axios.get('https://us-central1-open-data-cw2.cloudfunctions.net/getRankedNeedsDataNew').then(result=>{
+            console.log(result.data)
+        })
+    };
+
     return (<div>
         <div className="image-placeholder"> Image</div>
         <p>You can either search for foodbanks near you to donate to. Or, you can search for projects worldwide to donate to.</p>
         <div className="flex-gap" >
-            <Button type="primary" className="large-button">Search for foodbanks</Button>
+            <Button onClick={()=>{testApi();}} type="primary" className="large-button">Search for foodbanks</Button>
             <Button type="primary" className="large-button">Search for projects</Button>
         </div>
     </div>)
