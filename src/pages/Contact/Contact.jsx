4ec8d01bb5dc119a294befb6d4841a81e29907d4
wxk9 <wenxinlc@gmail.com>
2021-05-02 21:22:39 +0100

    Adjust contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,58 +1,59 @@
 import React, { useState } from "react";
 import "./Contact.css";
 import { Typography } from "antd";
+import Banner from "../../images/banner.jpeg";
 
 import { Form, Input, Button } from "antd";
 const { Title } = Typography;
 const { TextArea } = Input;
-// const FormSizeDemo = () => {
-//   const [componentSize, setComponentSize] = useState("default");
-// };
 
 export default function Contact() {
   return (
-    <div className="contact_content">
-      <div className="contact_text">
-        Our aim is to help everyone live a better life. We believe that, by
-        helping each other the world will be a better place to live. Together we
-        can remove the problems in this world such as hunger, poverty, lack of
-        education and many more! <br /> So join us in this mission to make the
-        world a better place to live in!{" "}
+    <div className="contact-content">
+      <div className="banner-section">
+        <img alt="banner" className="banner" src={Banner} />
+        <div className="banner-text">Contact Us</div>
       </div>
-      <div className="side_c">
-        <Title level={1} className="centering">
-          GET IN TOUCH
-        </Title>
 
-        {/* <div className="form_padding"> */}
+      <div className="contact-text">
+        <Typography.Text>
+          Our aim is to help everyone live a better life. We believe that, by
+          helping each other the world will be a better place to live. Together
+          we can remove the problems in this world such as hunger, poverty, lack
+          of education and many more! <br /> So join us in this mission to make
+          the world a better place to live in!
+        </Typography.Text>
+      </div>
+
+      <div className="form-content">
+        <Title level={1}>GET IN TOUCH</Title>
+
         <Form
           labelCol={{
             span: 4,
           }}
-          // wrapperCol={{
-          //   span: 16,
-          // }}
+          wrapperCol={{
+            span: 16,
+          }}
           layout="horizontal"
-          className="form_padding centering"
         >
           <Form.Item label="Name">
-            <Input />
+            <Input placeholder="Please enter your name" />
           </Form.Item>
           <Form.Item label="Email">
-            <Input />
+            <Input placeholder="Please enter your email" />
           </Form.Item>
           <Form.Item label="Subject">
-            <Input />
+            <Input placeholder="Please enter the subject" />
           </Form.Item>
           <Form.Item label="Message">
-            <TextArea rows={4} />
+            <TextArea rows={4} placeholder="Please enter your message" />
           </Form.Item>
 
-          <Form.Item className="centering">
+          <Form.Item className="submit-button">
             <Button>Submit</Button>
           </Form.Item>
         </Form>
-        {/* </div> */}
       </div>
     </div>
   );
